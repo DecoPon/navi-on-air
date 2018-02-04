16d7d80e3c4e1f2b2adb7ec6dbb55be6df80ecbb
DecoPon <indy12_dev@outlook.com>
2018-02-04 21:55:38 +0900

    test version

--- Browser_Handling.py
+++ Browser_Handling.py
@@ -39,12 +39,25 @@ class BrowserHandling:
         #driver = webdriver.Chrome(chrome_options=options)
 
         driver.get(url)
-
+        '''
         driver.find_element_by_id("lst-ib").send_keys("selenium")
         driver.find_element_by_id("lst-ib").send_keys(Keys.ENTER)
 
         driver.find_element_by_link_text("Selenium - Web Browser Automation")
+        '''
+
+        #driver.find_element_by_class_name('btn btn-github-inverse btn-block landingOauthSignup_button').send_keys(Keys.ENTER)
+        #driver.find_element_by_xpath('//*[@id="login"]/form/div[3]/input[3]').send_keys(Keys.ENTER)
+
+        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div[1]/div/a[1]').send_keys(Keys.ENTER)
+        driver.find_element_by_xpath('//*[@id="login_field"]').send_keys("")
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("")
+        driver.find_element_by_xpath('//*[@id="login"]/form/div[3]/input[3]').send_keys(Keys.ENTER)
+
+        driver.find_element_by_css_selector('div.st-Header_community').send_keys(Keys.ENTER)
 
         sleep(5)
+        driver.find_element_by_xpath('//*[@id="appRoot"]/div/div[1]/div/div[1]/div[3]/div[2]/a[1]').send_keys(Keys.ENTER)
 
+        sleep(5)
         driver.close()
